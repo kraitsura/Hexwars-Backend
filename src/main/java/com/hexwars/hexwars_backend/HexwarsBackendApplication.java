453af2c4ff093f2a4c53248def5aab0429dec57a
Aarya Reddy <b.aarya.reddy@gmail.com>
2024-06-15 18:12:20 -0700

    Initial commit

---
+++ src/main/java/com/hexwars/hexwars_backend/HexwarsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hexwars.hexwars_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexwarsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexwarsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexwars/hexwars_backend/HexwarsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexwars.hexwars_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexwarsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
