59c5216e87abe4d3003cb4885c59075649dbd1c5
Aarya Reddy <b.aarya.reddy@gmail.com>
2024-06-16 21:09:20 -0700

    Basic Model Build and architecture setup

--- src/main/java/com/hexwars/hexwars_backend/HexwarsBackendApplication.java
+++ src/main/java/com/hexwars/hexwars_backend/HexwarsBackendApplication.java
@@ -1,13 +1,34 @@
 package com.hexwars.hexwars_backend;
 
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HexwarsBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HexwarsBackendApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Bean Inspection:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
 	}
 
 }
